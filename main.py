22fcd99762ffddcaaee753ed55d0d0cacdeb88cc
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 23:27:27 +0530

    In Python we can use the del statement to remove one or more items from a list.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 
-# The clear() method removes all items from the list.
+# In Python we can use the del statement to remove one or more items from a list.
 
-prime_numbers = [2, 3, 5, 7, 9, 11]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# remove all elements
-prime_numbers.clear()
+# deleting the third item
+del my_list[2]
 
-# Updated prime_numbers List
-print('List after clear():', prime_numbers)
-# Output: List after clear(): []
+# Output: [1, 2, 4, 5, 6, 7, 8, 9]
+print(my_list)
 
+# deleting items from 2nd to 4th
+del my_list[1:4]
+
+# Output: [1, 6, 7, 8, 9]
+print(my_list)
+
+# deleting all elements
+del my_list[:]
+
+# Output: []
+print(my_list)
 
 
 
