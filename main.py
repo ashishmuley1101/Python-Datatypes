fc8fc4c01fa54a0e78ad6bb2699b5e7231531f40
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 19:34:19 +0530

    Python list append() method examples.

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 
-# Accessing Python List using for loop
+# Python list append() method
 
-name = ["krack", "jack", "tom", "jerry", "snappy", "richard"]
+# append() adding item on last index
 
-for x in name: # iterating all name list using 0 index to end
-    print(x)   # printing items using index
+name = ["krack", "tom", "jerry", "snappy", "richard"]
 
-    # O/P : krack
-    #       jack
-    #       tom
-    #       jerry
-    #       snappy
-    #       richard
+print("Before append = ", name)
+# O/P : Before append =  ['krack', 'tom', 'jerry', 'snappy', 'richard']
+
+name.append("jack")
+
+print("After append = ", name)
+# O/P : After append =  ['krack', 'tom', 'jerry', 'snappy', 'richard', 'jack']
+
+# ------------------append() adding two list------------------------------------
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+print("Before append = ", list1)  # O/P : Before append =  [1, 2, 3]
+
+list1.append(list2)
+
+print("After append = ", list1)  # O/P : After append =  [1, 2, 3, [4, 5, 6]]
+
+
+
+
 
 
 
