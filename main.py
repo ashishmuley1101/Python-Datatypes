6832e7d0eeecde584e4aa289f52a09ff2476f7cc
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 20:21:05 +0530

    The count() method returns the number of times the specified element appears in the list.

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 
-# The remove() method removes the first matching element (which is passed as an argument) from the list
+# The count() method returns the number of times the specified element appears in the list.
 
-# animals list
-animals = ['cat', 'dog', 'rabbit', 'pig']
-
-# 'rabbit' is removed
-animals.remove('rabbit')
-
-
-# Updated animals List
-print('Updated animals list: ', animals)
-# O/P : Updated animals list:  ['cat', 'dog', 'guinea pig']
-
-# list contains duplicate elements, the remove() method only removes the first matching element
 animals1 = ['cat', 'dog', 'dog', 'guinea pig', 'dog']
 
-# 'dog' is removed
-animals1.remove('dog')
+count = animals1.count('dog')
 
-
-# Updated animals list
-print('Updated animals1 list: ', animals1)
-# O/P : Updated animals1 list:  ['cat', 'dog', 'guinea pig', 'dog']
+print('Count for dog : ', count)
+# O/P : Count for dog : 3
 
 
 
