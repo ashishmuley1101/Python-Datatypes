78200024a754b49d4a965adfc5110c6707fe0657
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 20:37:14 +0530

    The pop() method removes the item at the given index from the list and returns the removed item.

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 
-# The remove() method removes the first matching element (which is passed as an argument) from the list
+# The pop() method removes the item at the given index from the list and returns the removed item.
 
-# animals list
-animals = ['cat', 'dog', 'rabbit', 'pig']
+animals1 = ['cat', 'dog', 'dog', 'pig', 'dog', 'goat', 'rabbit']
 
-# 'rabbit' is removed
-animals.remove('rabbit')
+pop_element = animals1.pop(2)
 
+print('Removed Element:', pop_element)
+print('Updated List:', animals1)
+# Output:
+# Removed Element: dog
+# Updated List: ['cat', 'dog', 'pig', 'dog']
 
-# Updated animals List
-print('Updated animals list: ', animals)
-# O/P : Updated animals list:  ['cat', 'dog', 'guinea pig']
+# ----------pop() without an index, and for negative indices----------
 
-# list contains duplicate elements, the remove() method only removes the first matching element
-animals1 = ['cat', 'dog', 'dog', 'guinea pig', 'dog']
+# remove and return the last item
+print('\nWhen index is not passed:')
+print('Return Value:', animals1.pop())
+print('Updated List:', animals1)
+# O/P :
+# When index is not passed:
+# Return Value: rabbit
+# Updated List: ['cat', 'dog', 'pig', 'dog', 'goat']
 
-# 'dog' is removed
-animals1.remove('dog')
+# remove and return the last item
+print('\nWhen -1 is passed:')
+print('Return Value:', animals1.pop(-1))
+print('Updated List:', animals1)
+# O/P :
+# When -1 is passed:
+# Return Value: goat
+# Updated List: ['cat', 'dog', 'pig', 'dog']
 
+# remove and return the third last item
+print('\nWhen -3 is passed:')
+print('Return Value:', animals1.pop(-3))
+print('Updated List:', animals1)
+# O/P :
+# When -3 is passed:
+# Return Value: dog
+# Updated List: ['cat', 'pig', 'dog']
 
-# Updated animals list
-print('Updated animals1 list: ', animals1)
-# O/P : Updated animals1 list:  ['cat', 'dog', 'guinea pig', 'dog']
 
 
 
