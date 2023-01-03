a4adbee693a0ca8c840ee68eb11d7843688db105
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 19:43:33 +0530

    Python list extend() method example.

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 
-# Python list append() method
+# Python list extend() method
 
-# append() adding item on last index
-
-name = ["krack", "tom", "jerry", "snappy", "richard"]
-
-print("Before append = ", name)
-# O/P : Before append =  ['krack', 'tom', 'jerry', 'snappy', 'richard']
-
-name.append("jack")
-
-print("After append = ", name)
-# O/P : After append =  ['krack', 'tom', 'jerry', 'snappy', 'richard', 'jack']
+# extend() joining two list in single list
+# add items of a list (rather than the list itself) to another list, use the extend() method.
 
 # ------------------append() adding two list------------------------------------
 
@@ -23,6 +14,19 @@ print("Before append = ", list1)  # O/P : Before append =  [1, 2, 3]
 list1.append(list2)
 
 print("After append = ", list1)  # O/P : After append =  [1, 2, 3, [4, 5, 6]]
+
+#-------------------------------------------------------------------------------
+
+# To overcome the append() adding two list we use extends()
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+print("Before extend = ", list1)  # O/P : Before append =  [1, 2, 3]
+
+list1.extend(list2)
+
+print("After extend = ", list1)  # O/P : After append =  [1, 2, 3, 4, 5, 6]
 
 
 
