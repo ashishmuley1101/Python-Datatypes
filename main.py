cd29c8b7a82f9992aa2f389b804f0e5cd5685937
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 14:48:48 +0530

    Python Numeric Data Type.

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# Python Numeric Data Type
+
+num1 = 5
+print(num1, 'is of type', type(num1)) # type() return int data type
+
+num2 = 5.42
+print(num2, 'is of type', type(num2)) # type() return float data type
+
+num3 = 8+2j
+print(num3, 'is of type', type(num3)) # type() return complex data type
