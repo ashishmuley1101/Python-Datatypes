78cdc8685e9b4e0570339fc2caf6a3184ff7e063
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 20:04:43 +0530

    The insert() method inserts an element to the list at the specified index.

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 
-# The index() method returns the index of the specified element in the list
+# The insert() method inserts an element to the list at the specified index
 
-# alphabets list
-alphabets = ['a', 'e', 'i', 'o', 'g', 'l', 'i', 'u']
+# create a list of vowels
+vowel = ['a', 'e', 'i', 'u']
 
-# index of 'i' in alphabets
-index = alphabets.index('e')   # O/P : 1
+# 'o' is inserted at index 3 (4th position)
+vowel.insert(3, 'o')
 
-print('The index of e:', index)
+print('vowel are = ', vowel)
 
-# 'i' after the 4th index is searched
-index = alphabets.index('i', 4)    # O/P : 6
-
-print('The index of i:', index)
-
-# 'i' between 3rd and 5th index is searched
-# index = alphabets.index('i', 3, 5)   # O/P : Error!
-
-# 'i' between 3rd and 7th index is searched
-index = alphabets.index('i', 3, 7)
-
-print('The index of i:', index)   # O/P : 6
+# Output: vowel are =  ['a', 'e', 'i', 'o', 'u']
 
 
 
