4f406e3ef7359a8b59f4b221e3f218e6bbe38424
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 23:19:07 +0530

    The clear() method removes all items from the list..

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 
-# The copy() method returns a shallow copy of the list.
+# The clear() method removes all items from the list.
 
-# mixed list
-prime_numbers = [2, 3, 5]
+prime_numbers = [2, 3, 5, 7, 9, 11]
 
-# copying a list
-numbers = prime_numbers.copy()
+# remove all elements
+prime_numbers.clear()
 
-print('Copied List:', numbers)
-# Output: Copied List: [2, 3, 5]
-
-# ---------shallow copy using the slicing syntax-----------
-# copy() in new list not changing old list
-
-# mixed list
-list = ['cat', 0, 6.7]
-
-# copying a list using slicing
-new_list = list[:]
-
-# Adding an element to the new list
-new_list.append('dog')
-
-# Printing new and old list
-print('Old List:', list)
-print('New List:', new_list)
-# Output :
-# Old List: ['cat', 0, 6.7]
-# New List: ['cat', 0, 6.7, 'dog']
+# Updated prime_numbers List
+print('List after clear():', prime_numbers)
+# Output: List after clear(): []
 
 
 
