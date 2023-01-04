84cecf68a949e1cc0e423766e1bc138c57e75535
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 21:04:26 +0530

    Python String startswith() method

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
 
-# Python String replace() method
+# Python String startswith() method
 
-# The replace() method replaces each matching occurrence of a substring with another string.
+# The startswith() method returns True if a string starts with the
+# specified prefix(string). If not, it returns False
 
-text = 'bat and ball'
+# The syntax of startswith() is: str.startswith(prefix[, start[, end]])
+# start (optional) - Beginning position where prefix is to be checked within the string.
+# end (optional) - Ending position where prefix is to be checked within the string.
 
-# replace 'ba' with 'ca'
-replaced_text = text.replace('ba', 'ca')
-print(replaced_text)
-# Output: cat and call
+message = 'Python is fun'
 
-# replace() Syntax : str.replace(old, new [, count])
-# count (optional) - the number of times you want to replace the old substring with the new string.
+# check if the message starts with Python
+print(message.startswith('Python'))
+# Output: True
 
-song = 'cold, cold heart'
 
-# replacing 'cold' with 'hurt'
-print(song.replace('cold', 'hurt'))
+# -----startswith() Without start and end Parameters---
+text = "Python is easy to learn."
 
-song = 'Let it be, let it be, let it be, let it be'
+result = text.startswith('is easy')
+print(result)   # O/p : False
 
-# replacing only two occurrences of 'let'
-print(song.replace('let', "don't let", 2))
+result = text.startswith('Python is ')
+print(result)  # O/p : True
 
-# replacing 0 occurrences of 'let'
-print(song.replace('let', 'so', 0))
+# -----startswith() With start and end Parameters-----
+
+text1 = "Python programming is easy."
+
+# start parameter: 7
+# 'programming is easy.' string is searched
+result = text1.startswith('programming is', 7)
+print(result)  # O/p : True
+
+# start: 7, end: 18
+# 'programming' string is searched
+result = text1.startswith('programming is', 7, 18)
+print(result)  # O/p : False
+
+result = text1.startswith('program', 7, 18)
+print(result)  # O/p : True
+
+# ------startswith() With Tuple Prefix--------------
+
+text3 = "programming is easy"
+result = text3.startswith(('python', 'programming'))
+
+# prints True
+print(result)
+
+result = text3.startswith(('is', 'easy', 'java'))
+
+# prints False
+print(result)
+
+# With start and end parameter
+# 'is easy' string is checked
+result = text3.startswith(('programming', 'easy'), 12, 19)
+
+# prints False
+print(result)
 
 # Output :
-# hurt, hurt heart
-# Let it be, don't let it be, don't let it be, let it be
-# Let it be, let it be, let it be, let it be
+
+
 
 
 
