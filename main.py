f85571eef6f51da143cb97031cf647e3c6ba7c08
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 17:03:14 +0530

    Accessing String datatype in Python using indexing, Negative Indexing, slicing , assigning new string variable and Multiline String.

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 
-# creating python string datatype
+# Access String datatype in Python
 
-# create a string using double quotes
-string1 = "Python programming"
-print(string1)
+greet = 'hello'
 
-# create a string using single quotes
-string2 = 'Python programming'
-print(string2)
+# access 1st index element
+print(greet[1])   # O/p : e
 
+# ------Negative Indexing-----
 
+# access 4th last element
+print(greet[-2])  # O/p : l
+
+# Access a range of characters in a string by using the slicing " : "
+
+# access character from 1st index to 3rd index
+print(greet[1:4])  # O/p : ell
+
+# strings are immutable. That means the characters of a string cannot be changed
+
+message = 'Hello'
+# message[0] = 'Y'
+# print(message) # Error...! --> TypeError: 'str' object does not support item assignment
+
+# Assign the variable name(message) to a new string name variable
+
+message = 'Hello Friends'
+
+# assign new string to message variable
+message = 'How are you ?'
+
+print(message)   # O/p :  Hello Friends
+
+#-------- Python Multiline String ----------
+
+# multiline string using  -- """ text """ ---
+
+message = """
+Never gonna give you up
+Never gonna let you down
+"""
+
+print(message)
 
 
