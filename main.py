6fe3d23d34c49093927943c04c4efd4462982955
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 16:21:54 +0530

    Creating python string datatype.

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 
-# accessing tuple elements using count()
+# creating python string datatype
 
-letters = ('a', 'p', 'p', 'l', 'e', 1, 2, 3, 2, 9, 1, 7, 2)
+# create a string using double quotes
+string1 = "Python programming"
+print(string1)
 
-print("Count : ", letters.count('l'))
-print("Count : ", letters.count(2))
-print("Count : ", letters.count('p'))
+# create a string using single quotes
+string2 = 'Python programming'
+print(string2)
 
-# Output :
-# Count :  1
-# Count :  3
-# Count :  2
 
-# iterating through for loop the tuple
-for x in letters:
-    print(x)
-
-# Check if an Item Exists in Tuple return boolean value
-print('b' in letters)    # O/p : False
-print(2 in letters)      # O/p : True
 
 
