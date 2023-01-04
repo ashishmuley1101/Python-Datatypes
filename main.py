248c2e35def1715abb79446c3196fd47e6e6bd28
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 20:47:15 +0530

    Python String replace() method.

--- main.py
+++ main.py
@@ -1,55 +1,35 @@
 
-# Python String find() method
+# Python String replace() method
 
-# The find() method returns the index of first occurrence of the substring (if found).
-# If not found, it returns -1.
+# The replace() method replaces each matching occurrence of a substring with another string.
 
-message = 'Python is a fun programming language'
+text = 'bat and ball'
 
-# check the index of 'fun'
-print(message.find('fun'))
-# Output: 12
+# replace 'ba' with 'ca'
+replaced_text = text.replace('ba', 'ca')
+print(replaced_text)
+# Output: cat and call
 
-# The syntax of the find() method is: str.find(sub[, start[, end]] )
+# replace() Syntax : str.replace(old, new [, count])
+# count (optional) - the number of times you want to replace the old substring with the new string.
 
-# -------------find() With No start and end Argument-----------------
+song = 'cold, cold heart'
 
-quote = 'Let it be, let it be, let it be'
+# replacing 'cold' with 'hurt'
+print(song.replace('cold', 'hurt'))
 
-# first occurance of 'let it'(case sensitive)
-result = quote.find('let it')
-print("Substring 'let it':", result)
+song = 'Let it be, let it be, let it be, let it be'
 
-# find returns -1 if substring not found
-result = quote.find('small')
-print("Substring 'small ':", result)
+# replacing only two occurrences of 'let'
+print(song.replace('let', "don't let", 2))
 
-# How to use find()
-if (quote.find('be,') != -1):
-    print("Contains substring 'be,'")
-else:
-    print("Doesn't contain substring")
+# replacing 0 occurrences of 'let'
+print(song.replace('let', 'so', 0))
 
 # Output :
-# Substring 'let it': 11
-# Substring 'small ': -1
-# Contains substring 'be,'
-
-# -------------find() With start and end Arguments-------------------
-
-quote = 'Do small things with great love'
-
-# Substring is searched in 'hings with great love'
-print(quote.find('small things', 10))  # O/P : -1
-
-# Substring is searched in ' small things with great love'
-print(quote.find('small things', 2))   # O/P : 3
-
-# Substring is searched in 'hings with great lov'
-print(quote.find('o small ', 10, -1))  # O/P : -1
-
-# Substring is searched in 'll things with'
-print(quote.find('things ', 6, 20))    # O/P : 9
+# hurt, hurt heart
+# Let it be, don't let it be, don't let it be, let it be
+# Let it be, let it be, let it be, let it be
 
 
 
