f7450588ed6103829e4f700414675303788ead59
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 13:03:47 +0530

    Creating tuple datatype in python.

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 
-# In Python we can use the del statement to remove one or more items from a list.
+# In Python A tuple can have any number of items and they may be of different types (integer, float, list, string, etc.).
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Different types of tuples
 
-# deleting the third item
-del my_list[2]
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
 
-# Output: [1, 2, 4, 5, 6, 7, 8, 9]
-print(my_list)
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
 
-# deleting items from 2nd to 4th
-del my_list[1:4]
+# tuple with mixed datatypes
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
 
-# Output: [1, 6, 7, 8, 9]
-print(my_list)
+# nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
 
-# deleting all elements
-del my_list[:]
+# Output :
+# ()
+# (1, 2, 3)
+# (1, 'Hello', 3.4)
+# ('mouse', [8, 4, 6], (1, 2, 3))
 
-# Output: []
-print(my_list)
+# Creating a tuple with one element is a bit tricky and one element normal data type
+# We will need a trailing comma " , " to indicate that it is a tuple.
+
+var1 = ("hello")
+print(type(var1))  # O/P :  <class 'str'>
+
+var2 = 123
+print(type(var2))  # O/P :  <class 'int'>
+
+# Creating a tuple having one element
+var3 = ("hello",)
+print(type(var3))  # O/P :  <class 'tuple'>
+
+# Parentheses is optional
+var4 = "hello",
+print(type(var4))  # O/P : <class 'tuple'>
 
 
 
