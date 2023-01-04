107e1fd23d7ae730f71f20376227fc9cfc7836de
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 21:19:20 +0530

    Python String endswith() method

--- main.py
+++ main.py
@@ -1,67 +1,77 @@
 
-# Python String startswith() method
+# Python String endswith() method
 
-# The startswith() method returns True if a string starts with the
+# The endswith() method returns True if a string end with the
 # specified prefix(string). If not, it returns False
 
-# The syntax of startswith() is: str.startswith(prefix[, start[, end]])
+# The syntax of endswith() is: str.endswith(suffix[, start[, end]])
 # start (optional) - Beginning position where prefix is to be checked within the string.
 # end (optional) - Ending position where prefix is to be checked within the string.
 
 message = 'Python is fun'
 
-# check if the message starts with Python
-print(message.startswith('Python'))
+# check if the message ends with fun
+print(message.endswith('fun'))
+
 # Output: True
 
 
-# -----startswith() Without start and end Parameters---
+# -----endswith() Without start and end Parameters---
 text = "Python is easy to learn."
 
-result = text.startswith('is easy')
-print(result)   # O/p : False
+result = text.endswith('to learn')
+# returns False
+print(result)
 
-result = text.startswith('Python is ')
-print(result)  # O/p : True
+result = text.endswith('to learn.')
+# returns True
+print(result)
 
-# -----startswith() With start and end Parameters-----
+result = text.endswith('Python is easy to learn.')
+# returns True
+print(result)
 
-text1 = "Python programming is easy."
+# -----endswith() With start and end Parameters-----
+
+text1 = "Python programming is easy to learn."
 
 # start parameter: 7
-# 'programming is easy.' string is searched
-result = text1.startswith('programming is', 7)
-print(result)  # O/p : True
+# "programming is easy to learn." string is searched
+result = text1.endswith('learn.', 7)
+print(result)
 
-# start: 7, end: 18
-# 'programming' string is searched
-result = text1.startswith('programming is', 7, 18)
-print(result)  # O/p : False
+# Both start and end is provided
+# start: 7, end: 26
+# "programming is easy" string is searched
 
-result = text1.startswith('program', 7, 18)
-print(result)  # O/p : True
+result = text1.endswith('is', 7, 26)
+# Returns False
+print(result)
 
-# ------startswith() With Tuple Prefix--------------
+result = text1.endswith('easy', 7, 26)
+# returns True
+print(result)
 
-text3 = "programming is easy"
-result = text3.startswith(('python', 'programming'))
+# ------endswith() With Tuple Prefix--------------
+
+text = "programming is easy"
+result = text.endswith(('programming', 'python'))
+
+# prints False
+print(result)
+
+result = text.endswith(('python', 'easy', 'java'))
+
+#prints True
+print(result)
+
+# With start and end parameter
+# 'programming is' string is checked
+result = text.endswith(('is', 'an'), 0, 14)
 
 # prints True
 print(result)
 
-result = text3.startswith(('is', 'easy', 'java'))
-
-# prints False
-print(result)
-
-# With start and end parameter
-# 'is easy' string is checked
-result = text3.startswith(('programming', 'easy'), 12, 19)
-
-# prints False
-print(result)
-
-# Output :
 
 
 
