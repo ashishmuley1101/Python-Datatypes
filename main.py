cbdc1e0fa9701d7c4711728d743bba2af76ebfdd
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-04 13:43:21 +0530

    Accessing tuple elements using indexing.

--- main.py
+++ main.py
@@ -1,48 +1,10 @@
 
-# In Python A tuple can have any number of items and they may be of different types (integer, float, list, string, etc.).
+# accessing tuple elements using indexing
 
-# Different types of tuples
+letters = ('a', 'p', 'p', 'l', 'e', 1, 2, 3, 2, 9, 1, 7, 2)
 
-# Empty tuple
-my_tuple = ()
-print(my_tuple)
-
-# Tuple having integers
-my_tuple = (1, 2, 3)
-print(my_tuple)
-
-# tuple with mixed datatypes
-my_tuple = (1, "Hello", 3.4)
-print(my_tuple)
-
-# nested tuple
-my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
-print(my_tuple)
-
-# Output :
-# ()
-# (1, 2, 3)
-# (1, 'Hello', 3.4)
-# ('mouse', [8, 4, 6], (1, 2, 3))
-
-# Creating a tuple with one element is a bit tricky and one element normal data type
-# We will need a trailing comma " , " to indicate that it is a tuple.
-
-var1 = ("hello")
-print(type(var1))  # O/P :  <class 'str'>
-
-var2 = 123
-print(type(var2))  # O/P :  <class 'int'>
-
-# Creating a tuple having one element
-var3 = ("hello",)
-print(type(var3))  # O/P :  <class 'tuple'>
-
-# Parentheses is optional
-var4 = "hello",
-print(type(var4))  # O/P : <class 'tuple'>
-
-
-
+print(letters.index('l'))  # O/p : 3
+print(letters.index(2))    # O/p : 6
+print(letters.index('p'))  # O/p : 1
 
 
